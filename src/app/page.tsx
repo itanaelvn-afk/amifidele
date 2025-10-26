6112aa4ca09f567d6a16adc6a27bc7e2f0dc69d5
itanaelvn-afk <itanael.vn@gmail.com>
2025-10-26 15:54:23 +0100

    Ajout Eslint, Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-amber-50">
-      <h1 className="text-4xl font-bold text-amber-800">Bienvenue sur AmiFidèle 🐾</h1>
+      <h1 className="text-4xl font-bold text-amber-800">
+        Bienvenue sur AmiFidèle 🐾
+      </h1>
     </div>
   );
 }
